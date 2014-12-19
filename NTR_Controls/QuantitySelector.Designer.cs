fce7f9e0e1f13e96d8707e466c29e0195f01d1ce
Leonardo Daga <leonardo.daga@gmail.com>
2014-12-19 21:20:45 +0100

    Aggiunta elementi esclusi da precedenti commit

---
+++ NTR_Controls/QuantitySelector.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NTR_Controls
+{
+    partial class QuantitySelector
+    {
+        /// <summary> 
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione componenti
+
+        /// <summary> 
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare 
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuantitySelector));
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "Star.png");
+            this.imageList.Images.SetKeyName(1, "HalfStar.png");
+            this.imageList.Images.SetKeyName(2, "DarkStar.png");
+            // 
+            // QuantitySelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "QuantitySelector";
+            this.Size = new System.Drawing.Size(384, 92);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.QuantitySelector_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList;
+
+
+    }
+}
---
+++ NTR_Controls/QuantitySelector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NTR_Controls
+{
+    public partial class QuantitySelector : UserControl
+    {
+        public QuantitySelector()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void ValueChangedHandler(float NewValue);
+        public event ValueChangedHandler ValueChanged;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Brush brBackground = new SolidBrush(this.BackColor);
+            e.Graphics.FillRectangle(brBackground, new Rectangle(0, 0, this.ClientSize.Width, this.ClientSize.Height));
+
+            RectangleF starRect = new RectangleF(0, 0, this.ClientSize.Width/5.0f, this.ClientSize.Height);
+
+            int i = 0;
+            for (; i <= Value - 1.0f; i++)
+            {
+                e.Graphics.DrawImage(imageList.Images[0], starRect);
+                starRect.Offset((float)(this.ClientSize.Width / 5.0f), 0f);
+            }
+
+            if (Value - i >= 0.5)
+            {
+                e.Graphics.DrawImage(imageList.Images[1], starRect);
+                starRect.Offset((float)(this.ClientSize.Width / 5.0f), 0f);
+                i++;
+            }
+
+            for (; i <= 5; i++)
+            {
+                e.Graphics.DrawImage(imageList.Images[2], starRect);
+                starRect.Offset((float)(this.ClientSize.Width / 5.0f), 0f);
+            }
+
+
+            brBackground.Dispose();
+        }
+
+        float _value = 0;
+        public float Value 
+        {
+            get { return _value; }
+
+            set
+            {
+                _value = value;
+                this.Invalidate();
+                if (ValueChanged != null)
+                    ValueChanged(_value);
+            } 
+        }
+
+        private void QuantitySelector_MouseClick(object sender, MouseEventArgs e)
+        {
+            float pos = (float)e.X / (float)this.ClientSize.Width;
+            pos = (float)((int)(pos * 10.0f)) / 2.0f + 0.5f;
+            Value = pos;
+        }
+    }
+}

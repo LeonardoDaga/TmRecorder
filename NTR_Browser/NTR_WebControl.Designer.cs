ac9927d74a36b7d34626d0b01dcc571fb4ee322b
Leonardo Daga <leonardo.daga@gmail.com>
2015-05-02 11:26:02 +0200

    Added NTR_WebBrowser project

--- NTR_Browser/NTR_WebControl.Designer.cs
+++ NTR_Browser/NTR_WebControl.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace A_TestForm
+﻿namespace NTR_WebBrowser
 {
-    partial class FormTest
+    partial class NTR_WebControl
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,26 +20,18 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // FormTest
-            // 
-            this.ClientSize = new System.Drawing.Size(670, 342);
-            this.Name = "FormTest";
-            this.ResumeLayout(false);
-
+            components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         }
 
         #endregion
-
-        private NTR_Controls.MatchStats matchStats1;
     }
 }
